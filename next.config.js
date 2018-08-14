cba67923cc6890d01b35b36c1758b1535220862c
John Lindquist <johnlindquist@gmail.com>
2018-08-14 15:20:38 -0600

    md-files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-const withMDX = require("@zeit/next-mdx")()
+const withMDX = require("@zeit/next-mdx")({
+  extension: /\.mdx?$/
+})
 module.exports = withMDX({
-  pageExtensions: ["js", "mdx"]
+  pageExtensions: ["js", "mdx", "md"]
 })
